b2358b00ee8f5553a55954e92204c4362dd78dd2
Tongruishan <416267068@qq.com>
2019-05-23 18:11:03 +0800

    Zinx V0.4 增加conf配置文件

---
+++ zinx/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"io/ioutil"
+	"encoding/json"
+	"fmt"
+)
+
+type Globle struct {
+	Name string
+	Host string
+	Port int
+	Version string
+	MaxLen int
+}
+
+var GlobleConf *Globle
+
+func(g *Globle)LoadConfig(){
+	data,err:=ioutil.ReadFile("conf/zinx.json")
+	if err!=nil{
+		fmt.Println("LoadConfig err",err)
+		return
+	}
+
+	err=json.Unmarshal(data,&GlobleConf)
+	if err!=nil{
+		fmt.Println("LoadConfig Unmarshal err",err)
+		return
+	}
+
+}
+
+func init(){
+
+	GlobleConfInit:=&Globle{
+		Name:"zinxSever",
+		Host:"0.0.0.0",
+		Port:8999,
+		Version:"Zinx V0.4",
+		MaxLen:4096,
+	}
+
+	GlobleConfInit.LoadConfig()
+
+}
+
